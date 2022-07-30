85d6d0242ae7e0ddf452d8560d2a3bdd42b5018b
Octavio Villena <octaviovillena@hotmail.com>
2022-07-29 22:24:53 -0700

    chat/game button styling

--- components/Game/index.tsx
+++ components/Game/index.tsx
@@ -58,6 +58,7 @@ export default observer(function game({ store, chatStore }: any) {
   return (
     <div className="flex items-start bg-gray-700">
       <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-3"
         onClick={() => {
           setIsChatMode(!isChatMode)
         }}
