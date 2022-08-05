f93adaef38335a18eae34699ecab2adba0574cea
Octavio Villena <octaviovillena@hotmail.com>
2022-08-04 20:26:42 -0700

    restored interfaces

--- Interfaces.ts
+++ Interfaces.ts
@@ -5,3 +5,62 @@ export interface IGuessComp {
 }
 
 export interface IJoinRoomProps {}
+
+export interface IStartGameOptions {
+  start: boolean
+  turn: boolean
+  player: number // corresponds to player number, could be username at some point
+}
+
+export interface IGameStoreInfo {
+  word: string
+  guesses: Array<string>
+  currentGuess: number
+  isInRoom: boolean
+  roomName: string
+  isPlayerTurn: boolean
+}
+export interface IGameStore extends IGameStoreInfo {
+  isGameStarted: boolean
+  player: number
+  won(): boolean
+  lost(): boolean
+  allGuesses(): Array<string>
+  exactGuesses(): Array<string> | null
+  inexactGuesses(): Array<string>
+  setWord(word: string): void
+  setGuesses(guesses: string[]): void
+  setCurrentGuess(currentGuess: number): void
+  setIsInRoom(isInRoom: boolean): void
+  setRoomName(roomName: string): void
+  setIsPlayerTurn(isPlayerTurn: boolean): void
+  setIsGameStarted(bool: boolean): void
+  setPlayer(player: number): void
+  init(): void
+  submitGuess(): void
+  handleInput(e: React.KeyboardEvent | React.BaseSyntheticEvent): void
+}
+
+export interface IChatData {
+  roomName: string
+  author: number //corresponds to the player number for now. Could change it to username at some point
+  message: string
+  time: string
+}
+export interface IChatInfo {
+  roomName: string
+  chat: Array<IChatData>
+}
+export interface IChatStore {
+  chat: Array<IChatData>
+  setChat(newChatData: Array<IChatData>): void
+  submitMessage(username: string, roomName: string, messageData: any): void
+}
+
+export interface IStores {
+  gameStore: IGameStore
+  chatStore: IChatStore
+}
+export interface IOnlyGameStore {
+  gameStore: IGameStore
+}
