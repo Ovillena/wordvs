98d59a3a724eb25fcad08864392e942668f02ca6
Octavio Villena <octaviovillena@hotmail.com>
2022-08-04 21:06:29 -0700

    add interfaces to chat component

--- components/Chat/index.tsx
+++ components/Chat/index.tsx
@@ -4,8 +4,9 @@ import gameService from '../../services/gameService'
 import chatService from '../../services/chatService'
 import socketService from '../../services/socketService'
 import ScrollToBottom from 'react-scroll-to-bottom'
+import { IChatData, IChatInfo, IStores } from '../../Interfaces'
 
-export default observer(function chat({ gameStore, chatStore }: any) {
+export default observer(function chat({ gameStore, chatStore }: IStores) {
   const [currentMessage, setCurrentMessage] = useState('')
   const [messageList, setMessageList] = useState([])
 
@@ -29,59 +30,36 @@ export default observer(function chat({ gameStore, chatStore }: any) {
         currentTime.getSeconds(),
     }
     if (currentMessage !== '') {
-      chatStore.submitMessage(gameStore.player, gameStore.roomName, messageData)
+      chatStore.submitMessage(
+        gameStore.player.toString(),
+        gameStore.roomName,
+        messageData
+      )
     }
   }
 
   const handleChatUpdate = () => {
     if (socketService.socket) {
-      chatService.onChatUpdate(socketService.socket, (newChatInfo: any) => {
-        console.log('new chat info : ', JSON.stringify(newChatInfo.chat))
-        // add player turn
-        chatStore.setChat(newChatInfo.chat)
-      })
+      chatService.onChatUpdate(
+        socketService.socket,
+        (newChatInfo: IChatInfo) => {
+          console.log('new chat info : ', JSON.stringify(newChatInfo.chat))
+          // add player turn
+          chatStore.setChat(newChatInfo.chat)
+        }
+      )
     }
   }
 
-  //   const handleGameUpdate = () => {
-  //     if (socketService.socket) {
-  //       chatService.onChatUpdate(socketService.socket, (newStoreInfo: any) => {
-  //         console.log('new store info : ', newStoreInfo)
-  //         // add player turn
-  //         store.setWord(newStoreInfo.word)
-  //         store.setGuesses(newStoreInfo.guesses)
-  //         store.setCurrentGuess(newStoreInfo.currentGuess)
-  //         store.setIsInRoom(newStoreInfo.isInRoom)
-  //         store.setRoomName(newStoreInfo.roomName)
-  //         store.setIsPlayerTurn(newStoreInfo.turn)
-  //       })
-  //     }
-  //   }
-
   return (
     <>
-      {/* <div className="chat-header">
-        <p>Live Chat</p>
-      </div>
-      <div className="chat-body"></div>
-      <div className="chat-footer">
-        <input
-          type="text"
-          className="chat-input"
-          onChange={(event) => {
-            setCurrentMessage(event.target.value)
-          }}
-        ></input>
-        <button onClick={handleSendMessage}>&#9658;</button>
-      </div> */}
-
       <div className="chat-window">
         <div className="chat-header">
           <p>Live Chat</p>
         </div>
         <div className="chat-body">
           <ScrollToBottom className="message-container">
-            {messageList.map((messageContent) => {
+            {messageList.map((messageContent: IChatData) => {
               return (
                 <div
                   className="message"
